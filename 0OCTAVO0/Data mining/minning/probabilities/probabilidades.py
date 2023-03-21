93d15902a84e14f74859263e5577305d2f09164d
ChrisRojano08 <crisrojano8@gmail.com>
2023-03-21 11:03:33 -0600

    [ChrisRojano08] 11:03 2103/2023

--- 0OCTAVO0/Data mining/minning/probabilities/probabilidades.py
+++ 0OCTAVO0/Data mining/minning/probabilities/probabilidades.py
@@ -16,7 +16,7 @@ def appendColms(contTbl):
     
     return dfSharp
 
-dataframe = pd.read_excel('input.xlsx', sheet_name='data')
+dataframe = pd.read_excel('input.xlsx', sheet_name='data1')
 dataRow = dataframe.iloc[:, 0]
 dataColm = dataframe.columns.ravel()
 dataframe = dataframe.iloc[: , 1:]
@@ -43,7 +43,7 @@ dfSharp = appendColms(contTbl)
 colms = list(dataColm)
 colms.append('Total')
 df = pd.DataFrame(dfSharp, columns=colms)
-with pd.ExcelWriter('output.xlsx',mode='w') as writer:  
+with pd.ExcelWriter('output1.xlsx',mode='w') as writer:  
     df.to_excel(writer, sheet_name='Contingencia')
 
 probTbl = np.empty([len(contTbl), len(contTbl[0])])
@@ -53,5 +53,28 @@ for i in range(len(contTbl)):
 
 dfSharp = appendColms(probTbl)
 df = pd.DataFrame(dfSharp, columns=colms)
-with pd.ExcelWriter('output.xlsx',mode='a', engine='openpyxl') as writer:  
+with pd.ExcelWriter('output1.xlsx',mode='a', engine='openpyxl') as writer:  
     df.to_excel(writer, sheet_name='Probabilidad', float_format="%.2f")
+    
+probMargin = np.empty([len(contTbl)-1, len(contTbl[0])])
+for i in range(len(contTbl)-1):
+    for j in range(len(contTbl[i])):
+        probMargin[i][j] = ''+str(contTbl[i][j]/contTbl[i][len(contTbl)-1])
+        
+dfSharp=[]
+for i in range(len(probMargin)):
+    a=[dataRow[i]]
+    a.extend(probMargin[i])
+    dfSharp.append(a)
+df = pd.DataFrame(dfSharp, columns=colms)
+with pd.ExcelWriter('output1.xlsx',mode='a', engine='openpyxl') as writer:  
+    df.to_excel(writer, sheet_name='Probabilidad Marginal 1', float_format="%.2f")
+    
+probMargin = np.empty([len(contTbl), len(contTbl[0])-1])
+for i in range(len(contTbl)):
+    for j in range(len(contTbl[i])-1):
+       probMargin[i][j] = ''+str(contTbl[i][j]/contTbl[len(contTbl)-1][j])  
+dfSharp = appendColms(probMargin)
+df = pd.DataFrame(dfSharp, columns=colms[:-1])
+with pd.ExcelWriter('output1.xlsx',mode='a', engine='openpyxl') as writer:  
+    df.to_excel(writer, sheet_name='Probabilidad Marginal 2', float_format="%.2f")
