643270d242a5a10a8174471f34b6eef5c756d894
ChrisRojano08 <crisrojano8@gmail.com>
2023-05-31 18:33:10 -0600

    31/05/2023 18:33

--- 0OCTAVO0/Data mining/minning/grafico/test.py
+++ 0OCTAVO0/Data mining/minning/grafico/test.py
@@ -12,8 +12,12 @@ def linea_curva(point1, point2):
     return (x,y)
 
 
-adV = [ [0,1,1,1],  [0,0,0,0],  [0,0,0,0] ]
-nodes = ['a','b','c','d']
+adV = [ [0,0,0,0,0],
+        [0,0,0,0,0],
+        [1,0,0,0,0], 
+        [1,0,0,0,0] ]
+rows = ['Contact','b','c','d']
+cols = ['a1','b1','c1','d1','e1']
 
 count = 0
 for i in range(len(adV)):
@@ -22,36 +26,56 @@ for i in range(len(adV)):
             count+=1
 
 points = {}
-justPoints = []
+justPointsR = []
+justPointsC = []
 ys = 0
 xs = 0
+count= count*2
 # Generar los puntos
-for i in range(len(nodes)):
+for i in range(len(rows)):
     x = random.uniform(0, count)
     y = random.uniform(0, count)
-    points[nodes[i]] = [x,y]
-    justPoints.append([x,y])
+    #points[rows[i]] = [x,y]
+    #justPointsR.append([x,y])
 
-    #points[nodes[i]] = [xs,ys]
-    #justPoints.append([xs,ys])
+    points[rows[i]] = [xs,ys]
+    justPointsR.append([xs,ys])
 
     ys+=1
-    xs+=0.5
+    xs+=1.5
+
+# Generar los puntos
+for i in range(len(cols)):
+    x = random.uniform(0, count)
+    y = random.uniform(0, count)
+    #points[cols[i]] = [x,y]
+    #justPointsC.append([x,y])
+
+    points[cols[i]] = [xs,ys]
+    justPointsC.append([xs,ys])
+
+    ys+=1
+    xs+=1.5
 
 # Graficando puntos y añadiendoles etiqueta
-plt.scatter([x[0] for x in justPoints], [x[1] for x in justPoints], s=500)
-for i in range(len(nodes)):
-    plt.text(justPoints[i][0], justPoints[i][1]+0.05, nodes[i], va='bottom', ha='center')
+plt.scatter([x[0] for x in justPointsR], [x[1] for x in justPointsR], s=500)
+plt.scatter([x[0] for x in justPointsC], [x[1] for x in justPointsC], s=500)
+
+for i in range(len(rows)):
+    plt.text(justPointsR[i][0], justPointsR[i][1]+0.05, rows[i], va='bottom', ha='center')
+for i in range(len(cols)):
+    plt.text(justPointsC[i][0], justPointsC[i][1]+0.05, cols[i], va='bottom', ha='center')
 
 # Graficando las conexiones entre los puntos segun la matriz de adyacencia
 vertex = []
 for i in range(len(adV)):
     for j in range(len(adV[i])):
         if adV[i][j]==1:
-            plt.plot( [points[nodes[i]][0], points[nodes[j]][0]], [points[nodes[i]][1], points[nodes[j]][1]] )
+            #plt.plot( [points[rows[i]][0], points[cols[j]][0]], [points[rows[i]][1], points[cols[j]][1]] )
 
-            #x,y = linea_curva(points[nodes[i]], points[nodes[j]])
-            #plt.plot( x,y )
+            x,y = linea_curva(points[rows[i]], points[cols[j]])
+            plt.plot( x,y )
+
 
 # Mostrando grafica cuadrada
 plt.axis('square')
