abcb753ebe0c898a02c1e101e94ac755497ac1ac
ChrisRojano08 <crisrojano8@gmail.com>
2023-06-06 00:03:47 -0600

    06/06/2023 0:03

--- 0OCTAVO0/Data mining/minning/grafico/pruebaTooltip.py
+++ 0OCTAVO0/Data mining/minning/grafico/pruebaTooltip.py
@@ -1,29 +1,33 @@
 import matplotlib.pyplot as plt
 
-imagen_mostrada = None
+datos = [
+    ["John Doe", 35, "Developer"],
+    ["Jane Smith", 28, "Designer"],
+    ["Bob Johnson", 42, "Manager"],
+]
 
-def mostrar_imagen(event):
-    global imagen_mostrada
-    
-    if event.xdata is not None and event.ydata is not None:
-        x = event.xdata
-        y = event.ydata
+# Establecer los espaciados para las celdas de la tabla
+x_start = 0.5
+y_start = 0.5
+x_spacing = 1.5
+y_spacing = 0.5
 
-        if x > 3.8 and x < 4.2 and y > 7.8 and y < 8.2:
-            if imagen_mostrada is None:
-                imagen = plt.imread('toolImg.png')
-                imagen_mostrada = plt.imshow(imagen, extent=(x+0.2, x+2, y+0.2, y+2), zorder=10)
-                plt.show()
-        else:
-            if imagen_mostrada is not None:
-                imagen_mostrada.set_visible(False)
-                plt.draw()
-                imagen_mostrada = None
-
+# Crear la gráfica vacía
 fig, ax = plt.subplots()
 
-cid_motion = fig.canvas.mpl_connect('motion_notify_event', mostrar_imagen)
-plt.scatter(4,8, s=100)
-plt.axis([0, 15, 0, 15])
+# Mostrar los datos de la tabla utilizando plt.text()
+for i, fila in enumerate(datos):
+    for j, valor in enumerate(fila):
+        x = x_start + j * x_spacing
+        y = y_start + i * y_spacing
+        ax.text(x, y, str(valor), ha='left', va='center')
 
+# Establecer los límites del gráfico
+ax.set_xlim(0, x_start + len(datos[0]) * x_spacing)
+ax.set_ylim(0, y_start + len(datos) * y_spacing)
+
+# Ocultar los ejes
+ax.axis('off')
+
+# Mostrar la gráfica
 plt.show()
--- 0OCTAVO0/Data mining/minning/grafico/uwuw.py
+++ 0OCTAVO0/Data mining/minning/grafico/uwuw.py
@@ -1,8 +1,24 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from tabulate import tabulate
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 
 tablas_mostradas = {}
 tabla_mostrada = None
 
+# Funcion chida para agregar una curva
+def linea_curva(point1, point2):
+    a = (point2[1] - point1[1])/(np.cosh(point2[0]) - np.cosh(point1[0]))
+    b = point1[1] - a*np.cosh(point1[0])
+    x = np.linspace(point1[0], point2[0], 100)
+    y = a*np.cosh(x) + b
+
+    return (x,y)
+
+# Evento que detecta la posicion del mouse y si esta
+# en la lista de puntos se muestra la tabla
 def mostrar_tabla(event):
     global tablas_mostradas
 
@@ -21,27 +37,90 @@ def mostrar_tabla(event):
                 tabla.set_visible(False)
             plt.draw()
 
-
+#Añadir el evento del mouse al grafico
 fig, ax = plt.subplots()
-
 cid_motion = fig.canvas.mpl_connect('motion_notify_event', mostrar_tabla)
 
-puntos = [(4, 8), (5,9), (6,10)]
-datos_puntos = {
-    (4, 8): [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-    (5, 9): [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
-    (6, 10): [[19, 20, 21], [22, 23, 24], [25, 26, 27]],
-}
+#Leyendo matriz de adyacencia
+dataframe = pd.read_excel('matriz_adyacencia1.xlsx')
+dataColm = dataframe.columns.ravel()
+data = list(dataframe.values)
 
-for punto in puntos:
+# Dando formato a los datos para generar filas, columnas y matriz sola
+adV = tuple([x[1:] for x in data])
+rows = tuple([x[:1] for x in data])
+rows = [str(x) for x in rows]
+rows = [x.replace('\'','').replace(']','').replace('[','') for x in rows]
+cols = tuple(dataColm[1:])
+
+# Calculando numeros de puntos en la matriz
+# para definir el tamaño de la ventana
+count = 0
+for i in range(len(adV)):
+    for j in range(len(adV[i])):
+        if adV[i][j]==1:
+            count+=1
+
+points = {}
+justPointsR = []
+justPointsC = []
+ys = 2
+xs = 2
+
+# Generar los puntos
+for row in rows:
+    points[str(row)] = [xs,ys]
+    justPointsR.append([xs,ys])
+
+    ys+=1
+    xs+=1
+
+# Generar los puntos
+for col in cols:
+    points[str(col)] = [xs,ys]
+    justPointsC.append([xs,ys])
+
+    ys+=1
+    xs+=1
+
+# Graficando puntos
+plt.scatter([x[0] for x in justPointsR], [x[1] for x in justPointsR], s=500, color='#B0D9F2')
+plt.scatter([x[0] for x in justPointsC], [x[1] for x in justPointsC], s=500, color='#C1B0F2')
+
+# Añadiendo etiquetas al grafico
+for i in range(len(rows)):
+    plt.text(justPointsR[i][0], justPointsR[i][1]+0.05, rows[i], va='bottom', ha='center')
+for i in range(len(cols)):
+    plt.text(justPointsC[i][0], justPointsC[i][1]+0.05, cols[i], va='bottom', ha='center')
+
+# Graficando las conexiones entre los puntos segun la matriz de adyacencia
+vertex = []
+for i in range(len(adV)):
+    for j in range(len(adV[i])):
+        if adV[i][j]==1:
+            x,y = linea_curva(points[str(rows[i])], points[str(cols[j])])
+            plt.plot( x,y, 'k')
+
+# Generar las tablas para los puntos de las filas
+for punto in justPointsR:
     x, y = punto
-    datos = datos_puntos[punto]
-    formato_tabla = '\n'.join([' '.join(map(str, fila)) for fila in datos])
+    punto = (punto[0], punto[1])
+    formato_tabla = '\n'.join([' | '.join(map(str, fila)) for fila in adV])
     tabla_mostrada = plt.text(x, y, formato_tabla, ha='left', va='top', bbox=dict(facecolor='white', alpha=0.8), visible=False)
     tablas_mostradas[punto] = tabla_mostrada
 
-plt.scatter(4, 8)
-plt.scatter(5, 9)
-plt.scatter(6, 10)
-plt.axis([0, 15, 0, 15])
+# Generar las tablas para los puntos de las columnas
+for punto in justPointsC:
+    x, y = punto
+    punto = (punto[0], punto[1])
+    formato_tabla = '\n'.join([' | '.join(map(str, fila)) for fila in adV])
+
+    tabla_mostrada = plt.text(x, y, formato_tabla, ha='left', va='top', bbox=dict(facecolor='white', alpha=0.8), visible=False)
+    tablas_mostradas[punto] = tabla_mostrada
+
+# Mostrando grafica cuadrada
+plt.axis('square')
+plt.axis([0, xs+2, 0, xs+2])
 plt.show()
+
+
